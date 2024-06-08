90564bc6570cbb94e083cafb5d7a8387b70f2b92
Weston <westonkelliher@gmail.com>
2024-06-07 19:36:14 -0700

    Make a wheel that turns nicely in response to touch-drags

--- controller/app.js
+++ controller/app.js
@@ -1,86 +1,216 @@
 import { send_controlpad_message } from "./controlpad.js";
 
-//////////////////////// HANDLE MESSAGES FROM GAME /////////////////////////////
 
-// TODO: handle messages from the game here
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-
-// set to true to enable logs from controlpad.js
-// make sure to set to false before submitting your code
-export const DEBUG = false;
-
-
-//////////////////////////////// Layout Elements ///////////////////////////////
-
-document.addEventListener("viewport-change", (event) => {
-    // set up the elements of your controller here. This will ensure proper
-    // behavior when players change orientation of their phone.
-    layoutElements(event.detail.isPortrait, event.detail.viewWidth,
-                   event.detail.viewHeight);
+document.addEventListener("controlpad-message", (event) => {
+    var msg = event.detail;
+    console.log("recv: " + msg);
+    let movePad = document.getElementById("wheelPad");
+    //movePad.style.background = msg;
+    //movePad.style.display = "none";
 });
 
-function layoutElements(isPortrait, viewWidth, viewHeight) {
-    // In the layout functionality below we simply switch which elements are
-    // visible (the positioning of portrait vs landscape elements is defined
-    // in css files in controller/styles.
-    //
-    // Optionally, you may also use a single element per item for both portrait
-    // and landscape and change its positioning in layout functions based on
-    // whether the view is portrait or landscape.
-    //
-    if (isPortrait) {
-        document.getElementById("container-portrait").style.display = "flex";
-        document.getElementById("container-landscape").style.display = "none";        
-    } else {
-        document.getElementById("container-portrait").style.display = "none";
-        document.getElementById("container-landscape").style.display = "flex";
+
+// Throttle function to limit the rate of execution
+function throttle(func, limit) {
+    var lastFunc;
+    var lastRan;
+    return function() {
+        const context = this;
+        const args = arguments;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function() {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+}
+
+
+// Movement
+let isTouchActive = false;
+let lastTouchAngle = 0.0;
+let newTouchAngle = 0.0;
+let wheelRotation = 0.0;
+var touchPad = document.getElementById('wheelPad');
+touchPad.style.width = "50px";
+touchPad.style.height = "50px";
+let stickPosition = {x: 0, y: 0};
+
+function angle_of_touch(x, y) {
+    const div = document.getElementById("wheelPad");
+    const rect = div.getBoundingClientRect();
+    const divX = rect.left + rect.width / 2;
+    const divY = rect.top + rect.height / 2;
+    const angle = Math.atan2(y - divY, x - divX);
+    return angle;
+}
+
+// as a fraction (0.0 to 1.0) (i.e. not in pixels)
+function dist_of_touch(x, y) {
+    const div = document.getElementById("wheelPad");
+    const rect = div.getBoundingClientRect();
+    const divX = rect.left + rect.width / 2;
+    const divY = rect.top + rect.height / 2;
+    const dX = x - divX;
+    const dY = y - divY;
+    const angle = Math.sqrt(dX*dX + dY*dY) / (rect.width/2);
+    return angle;
+    
+}
+
+
+touchPad.addEventListener('touchstart', function(e) {
+    // Prevent the default highlighting behavior
+    e.preventDefault();
+
+    if (isTouchActive) { return; }
+
+    // check for correct touch
+    var touch = null;
+    for (let aTouch of e.changedTouches) {
+        if (aTouch.target.id == "wheelPad") {
+            touch = aTouch;
+        }
+    }
+    if (touch == null) {
+        return;
+    }
+
+    isTouchActive = true;
+
+    // Get the touch position
+    var touchX = touch.clientX;
+    var touchY = touch.clientY;
+    var angle = angle_of_touch(touchX, touchY);
+    lastTouchAngle = angle;
+    newTouchAngle = angle;
+}, false);
+
+
+touchPad.addEventListener('touchend', function(e) {
+    isTouchActive = false;
+    send_controlpad_message("stick:0,0");
+}, false);
+
+touchPad.addEventListener('touchcancel', function(e) {
+    isTouchActive = false;
+    send_controlpad_message("stick:0,0");
+}, false);
+
+
+
+var throttledDragEvent = throttle(function(e) {
+    // check for correct touch
+    var touch = null;
+    for (let aTouch of e.changedTouches) {
+        if (aTouch.target.id == "wheelPad") {
+            touch = aTouch;
+        }
+    }
+    if (touch == null) {
+        return;
+    }
+    // Get the touch position
+    var touchX = touch.clientX;
+    var touchY = touch.clientY;
+
+    if (isTouchActive) {
+        lastTouchAngle = newTouchAngle;
+        newTouchAngle = angle_of_touch(touchX, touchY);
+        var angle_diff = newTouchAngle - lastTouchAngle;
+        console.log(angle_diff);
+        if (angle_diff > Math.PI) {
+            angle_diff -= Math.PI*2;
+        } else if (angle_diff < -Math.PI) {
+            angle_diff += Math.PI*2;
+        }
+        var dist = dist_of_touch(touchX, touchY);
+        var dist_multiplier = Math.min(dist*2, 1.0);
+        var rotation_amount = dist_multiplier*angle_diff;
+        wheelRotation += rotation_amount;
+        var rotation_degrees = wheelRotation * 180 / Math.PI;
+        var wheel = document.getElementById("wheel");
+        wheel.style.transform = `translate(-50%, -50%) rotate(${rotation_degrees}deg)`;
+        //send_controlpad_message(datum);
+    }
+}, 33); // Throttle to 30 times per second (33.33 milliseconds)
+
+// Add the throttled touchmove event listener to the element
+touchPad.addEventListener('touchmove', throttledDragEvent, false);
+//touchPad.addEventListener('touchmove', () => {console.log("f");}, false);
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+var windowWidth = 0;
+var windowHeight = 0;
+
+layoutElements();
+
+function dimsCheck() {
+    if (windowWidth != window.innerWidth || windowHeight != window.innerHeight) {
+        layoutElements();
     }
 }
 
+setInterval(dimsCheck, 100);
 
-////////////////////////////////////////////////////////////////////////////////
-
-// prevent usage of input box from causing elements to scroll out of view (this
-// is a default behavior we are essentially overriding)
-document.getElementById("input-text-portrait").addEventListener('blur', () => {
-    window.scrollTo(0, 0);
-});
-document.getElementById("input-text-landscape").addEventListener("blur", () => {
-    window.scrollTo(0,0);
-});
-
-/////////////////////////// EXAMPLE BUTTON LISTENER /////////////////////////////
-
-document.getElementById("enter-button").addEventListener("click", () => {
-    send_controlpad_message("hello");
-});
-
-document.getElementById("enter-button-landscape").addEventListener("click", () => {
-    send_controlpad_message("hello");
-});
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-// Add listeners for the text input fields to send messages to the game
-// If you're not using any text inputs you can remove the below function
-function setupInputListeners() {
-    document.getElementById('send-button').addEventListener('click', function() {
-        var inputText = document.getElementById('input-text-portrait').value;
-        send_controlpad_message(inputText);
-    });
-
-    document.getElementById('send-button-landscape').addEventListener('click', function() {
-        var inputText = document.getElementById('input-text-landscape').value;
-        send_controlpad_message(inputText);
-    });    
+function layoutElements() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    //var isPortrait = windowHeight > windowWidth;
+    //touchPad.style.width = "200%";
+    console.log("Asa");
+    //touchPad.style.height = "300%";
+    layoutMovePad();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    setupInputListeners(); // can remove this line if not using any text inputs
-});
-
+function layoutMovePad() {
+    var movePad = document.getElementById('wheelPad');
+    movePad.style.position = "absolute";
+    movePad.style.top = "50%";
+    movePad.style.left = "50%";
+    movePad.style.transform = "translate(-50%, -50%)";
+    movePad.style.width = "100vw";
+    movePad.style.height = "100vw";
+    //movePad.style.border = "1px solid #bbbbbb";
+    movePad.style.borderRadius = "140px";
+    //
+    var pad_img = document.createElement('img');
+    pad_img.style.position = "absolute";
+    pad_img.style.top = "50%";
+    pad_img.style.left = "50%";
+    pad_img.style.width = "100%";
+    pad_img.style.height = "100%";
+    pad_img.style.transform = "translate(-50%, -50%)";
+    pad_img.style.pointerEvents = "none";
+    pad_img.src = "./resources/wheel.png";
+    pad_img.id = "wheel";
+    console.log(pad_img.id);
+    //pad_img.style.display = "none";
+    //
+    var center_img = document.createElement('img');
+    center_img.style.position = "absolute";
+    center_img.style.top = "50%";
+    center_img.style.left = "50%";
+    center_img.style.width = "100%";
+    center_img.style.height = "100%";
+    center_img.style.transform = "translate(-50%, -50%)";
+    center_img.style.pointerEvents = "none";
+    center_img.src = "./resources/center.png";
+    //center_img.style.display = "none";
+    //
+    while (movePad.firstChild) {
+        movePad.removeChild(movePad.firstChild);
+    }
+    movePad.appendChild(pad_img);
+    movePad.appendChild(center_img);
+}
 
